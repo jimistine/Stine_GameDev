032be350220eae0ff22a39d7ff9ee0e93a1e6f95
jimistine <jrstine@me.com>
2017-09-13 12:16:35 -0400

    Added cameras, camera behavior, player, lateral player movement
    and stopped there at the end of the first lab.

---
+++ Stine_GameDev/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour {
+
+	public GameObject Cam1;
+	public GameObject Cam2;
+	public GameObject Cam3;
+ 
+	void Start() {
+		Cam1.SetActive(true);
+		Cam2.SetActive(false);
+		Cam3.SetActive(false);
+	}
+ 
+	void Update() {
+ 
+		if (Input.GetKeyDown(KeyCode.I)) {
+			Cam1.SetActive(true);
+			Cam2.SetActive(false);
+			Cam3.SetActive(false);
+		}
+		if (Input.GetKeyDown(KeyCode.O)) {
+			Cam1.SetActive(false);
+			Cam2.SetActive(true);
+			Cam3.SetActive(false);
+		}
+		if (Input.GetKeyDown(KeyCode.P)) {
+			Cam1.SetActive(false);
+			Cam2.SetActive(false);
+			Cam3.SetActive(true);
+		}
+	}
+}
---
+++ Stine_GameDev/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public Rigidbody2D PlayerRigidbody2D;
+	public float MoveSpeed;
+
+	void Update()
+	{
+		if (Input.GetAxis("Horizontal") < 0)
+		{
+			PlayerRigidbody2D.AddForce(Vector2.left * MoveSpeed);
+		}
+		if (Input.GetAxis("Horizontal") > 0)
+		{
+			PlayerRigidbody2D.AddForce(Vector2.right * MoveSpeed);
+		}
+	}
+}
