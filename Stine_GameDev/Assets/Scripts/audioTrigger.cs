1e52c3455a26c9beee0e0272158d44249af0d656
jimistine <jrstine@me.com>
2017-09-27 11:04:14 -0400

    Added exit door, robo, first iteration of robovoice
    these were the first changes I made in lab.

---
+++ Stine_GameDev/Assets/Scripts/audioTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioTrigger : MonoBehaviour
+{
+
+	public Collider2D audioCollider;
+	private bool audioTrue;
+	
+
+	private void OnTriggerStay2D(Collider2D other)
+	{
+		if (Input.GetKeyDown(KeyCode.Return) && audioTrue)
+		{
+			Debug.Log("speaking");
+			GetComponent<AudioSource> ().Play ();
+			audioTrue = false;
+		}
+	}
+}
