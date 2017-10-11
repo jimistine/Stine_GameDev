bcab5f4cc61f8975d7498a3414bd70a22edb9f8e
jimistine <jrstine@me.com>
2017-10-11 12:20:20 -0400

    Built for web and as an excecutable,
    don't think much else changed.

--- Stine_GameDev/Assets/Scripts/GM.cs
+++ Stine_GameDev/Assets/Scripts/GM.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
-using NUnit.Framework.Constraints;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
