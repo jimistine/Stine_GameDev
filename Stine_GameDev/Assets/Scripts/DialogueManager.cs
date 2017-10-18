7e9344e3cde2619c0184775f49698117b51de970
jimistine <jrstine@me.com>
2017-10-18 18:31:23 -0400

    Added dialogue system test scene,
    there's a dialogue system in there too.

---
+++ Stine_GameDev/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,69 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+ using System.Security.Cryptography.X509Certificates;
+ using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+	public Text nameText;
+	public Text dialogueText; 
+	
+	private Queue<string> sentences;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		sentences = new Queue<string>();
+	}
+
+	public void StartDialogue(Dialogue dialogue)
+	{
+//		Debug.Log("Starting Convo with " + dialogue.Name);
+
+		nameText.text = dialogue.Name;
+		
+		sentences.Clear();
+		
+		foreach (string sentence in dialogue.sentences)
+		{
+			sentences.Enqueue(sentence);
+		}
+
+		DisplayNextSentence();
+
+	}
+
+	public void DisplayNextSentence()
+	{
+		if (sentences.Count == 0)
+		{
+			EndDialogue();
+			return;
+		}
+
+		string sentence = sentences.Dequeue();
+		StopAllCoroutines();
+		StartCoroutine(TypeSentence (sentence));
+//		dialogueText.text = sentence;
+//		Debug.Log(sentence);
+	}
+
+	IEnumerator TypeSentence(string sentence)
+	{
+		dialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogueText.text += letter;
+			yield return null;
+		}
+	}
+	
+
+	void EndDialogue()
+	{
+		Debug.Log("End of conversation ");
+		
+	}
+}
