b1c06a3a78783352b6dec59ad0b1632951a1ef65
jimistine <jrstine@me.com>
2017-10-25 10:46:36 -0400

    Added an actual fireplace, spright, particle system,
    I'm so incredibly tired.

--- Stine_GameDev/Assets/Scripts/PlayerController.cs
+++ Stine_GameDev/Assets/Scripts/PlayerController.cs
@@ -23,18 +23,13 @@ public class PlayerController : MonoBehaviour
 
 	void Update()
 	{
-		if (ani_this.GetBool("ifInteract") == false && Input.GetKeyDown(KeyCode.A) || 
-		    ani_this.GetBool("ifInteract") == false && Input.GetKey(KeyCode.LeftArrow))
+		if (ani_this.GetBool("ifInteract") == false && Input.GetKeyDown(KeyCode.LeftArrow) || 
+			ani_this.GetBool("ifInteract") == false && Input.GetKeyDown(KeyCode.A))
 		{
 			ani_this.SetBool ("ifWalkingLeft", true);
 			ani_this.SetBool ("ifLeft", true);
 			GM.Me.Footsteps.Play();
 			PlayerRigidbody2D.velocity = -MoveSpeed * move;
-//			if (FacingLeft == false)
-//			{
-//				transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-//				FacingLeft = true;
-//			}
 		}
 		if (Input.GetKeyUp (KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow)) 
 		{
@@ -50,17 +45,13 @@ public class PlayerController : MonoBehaviour
 			ani_this.SetBool ("ifLeft", false);
 			GM.Me.Footsteps.Play();
 			PlayerRigidbody2D.velocity = MoveSpeed * move;
-//			if (FacingLeft)
-//			{
-//				transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-//				FacingLeft = false;
-//			}
 		}
 		if (Input.GetKeyUp (KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow)) 
 		{
 			ani_this.SetBool ("ifWalking", false);
 			PlayerRigidbody2D.velocity = new Vector2(0, 0);
 			GM.Me.Footsteps.Stop();
+			
 		}
 		if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.LeftShift))
 		{
@@ -72,21 +63,5 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 }
-
-
-//Old Movement
-//		if (Input.GetAxis("Horizontal") < 0)
-//		{
-//			PlayerRigidbody2D.AddForce(Vector2.left * MoveSpeed);
-//		}
-//		if (Input.GetAxis("Horizontal") > 0)
-//		{
-//			PlayerRigidbody2D.AddForce(Vector2.right * MoveSpeed);
-//		}
-
-//		if (Input.GetKeyDown(KeyCode.Return))
-//		{
-//			Interact();
-//		}
 	
 
