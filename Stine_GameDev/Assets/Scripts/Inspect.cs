69ebcdd0721e9956df9743115176622c3c8627f6
jimistine <jrstine@me.com>
2017-11-07 16:06:41 -0500

    Closes #6
    added a cam for the books/notes etc in rm 2,3

--- Stine_GameDev/Assets/Scripts/Inspect.cs
+++ Stine_GameDev/Assets/Scripts/Inspect.cs
@@ -8,6 +8,7 @@ public class Inspect : MonoBehaviour
 
 	public GameObject Iring;
 	public GameObject InspectResult;
+	public GameObject BookCam;
 	void Update()
 	{	
 		//Close Inspect
@@ -22,6 +23,7 @@ public class Inspect : MonoBehaviour
 	{
 		Iring.SetActive(false);
 		InspectResult.SetActive(false);
+		BookCam.SetActive(false);
 		GM.Me.ExitInspect();
 	}
 	private void OnTriggerStay2D(Collider2D other)
@@ -29,7 +31,9 @@ public class Inspect : MonoBehaviour
 		if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.LeftShift))
 		{
 			InspectResult.SetActive(true);
+			BookCam.SetActive(true);
 			GM.Me.Inspecting = true;
+			
 			if (gameObject.CompareTag("book"))
 			{
 				GM.Me.OpenBook.Play();
@@ -45,8 +49,11 @@ public class Inspect : MonoBehaviour
 			if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) ||
 			    Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
 			{
+				
 				GM.Me.ExitInspect();
 				InspectResult.SetActive(false);
+				BookCam.SetActive(false);
+
 				if (gameObject.CompareTag("book"))
 				{
 					GM.Me.CloseBook.Play();
